384b21b8c54ba1c62da47d4a966868a72cd8ecd7
Parker Levin <pedker@gmail.com>
2021-01-27 06:40:00 -0800

    Accomodate Site Updates; Error Proofing

--- scraper.py
+++ scraper.py
@@ -1,25 +1,69 @@
-import urllib.request
+import time
+import urllib
 import requests
+import pathlib
 from bs4 import BeautifulSoup
 
-exclusion = ['Random', 'Games', 'Donate', 'Contact', 'Tweet', 'Buy this Game', 'Buy the official music album']
-reservedChars = ['/', '\\', '?', '%', '*', ':', '|', '"', '<', '>']
+EXCLUSION = ['Random', 'Games', 'Switch Theme', 'Log In', 'About', 'Donate', 'Contact', 'Tools', 'Tweet', 'Buy this Game', 'Buy the official music album', 'Preferences']
+RESERVED_CHARS = ['/', '\\', '?', '%', '*', ':', '|', '"', '<', '>', '\t']
+DOWNLOAD_BUFFER = 15
+BASE_DOWNLOAD_URL = "https://smashcustommusic.net/bcstm/"
 
-baseDownloadURL = "http://smashcustommusic.com/bcstm"
-downloadLocation = str(input("Download Location: "))
-gameURL = "http://smashcustommusic.com/game/" + str(input('Game Code: '))
 
-response = requests.get(gameURL)
-soup = BeautifulSoup(response.text, 'html.parser')
-entries = soup.findAll('a')
 
-for entry in entries:
-    if entry.get_text() not in exclusion:
-        href = entry['href']
-        fileName = entry.get_text() + '.bcstm'
-        for char in reservedChars:
-            fileName = fileName.replace(char, '_')
-        
-        print(f'Downloading {fileName} ...')
-        page = urllib.request.urlretrieve(baseDownloadURL+href, downloadLocation+'/'+fileName)
+def recursiveScrape(index: int):
+    if index >= len(entries):
+        return;
+    if  entries[index].get_text() in EXCLUSION:
+        recursiveScrape(index + 1)
+    else:
+        href = entries[index]['href'][6:];
+        fileName = entries[index].get_text() + '.bcstm';
+        for char in RESERVED_CHARS:
+            fileName = fileName.replace(char, '_');
 
+        print(f'Downloading {fileName}... ', end='');
+        try:
+            page = urllib.request.urlretrieve(BASE_DOWNLOAD_URL+href, downloadLocation+'/'+fileName);
+            print("Done. Waiting...");
+            time.sleep(DOWNLOAD_BUFFER);
+            recursiveScrape(index + 1);
+        except urllib.error.HTTPError as e:
+            print("An error occured {", str(e), "}. Retrying in ", DOWNLOAD_BUFFER, " seconds...");
+            time.sleep(DOWNLOAD_BUFFER);
+            recursiveScrape(index);
+
+            
+
+
+if __name__ == '__main__':
+    downloadLocation = pathlib.Path(str(input("Download Location: ")))
+    while not downloadLocation.is_dir():
+        downloadLocation = pathlib.Path(str(input("That didn't work. Try again: \n")))
+    downloadLocation = str(downloadLocation)
+    gameURL = "https://smashcustommusic.net/game/" + str(input('Game Code: '))
+    response = requests.get(gameURL)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    entries = soup.findAll('a')
+    recursiveScrape(0)
+    x = input("\nCompleted successfully. Press ENTER.");
+
+
+# OLD
+
+#try:
+#    for entry in entries:
+#        if entry.get_text() not in EXCLUSION:
+#            href = entry['href'][6:]
+#            fileName = entry.get_text() + '.bcstm'
+#            for char in RESERVED_CHARS:
+#                fileName = fileName.replace(char, '_')
+            
+#            print(f'Downloading {fileName}...')
+#            page = urllib.request.urlretrieve(BASE_DOWNLOAD_URL+href, downloadLocation+'/'+fileName)
+#            time.sleep(DOWNLOAD_BUFFER)
+#    x = input("\nCompleted successfully. Press ENTER.");
+#except urllib.error.HTTPError as e:
+#    print("An error occured {", str(e), "}. Retrying in ", 90, " seconds...")
+#    time.sleep(90)
+#    page = urllib.request.urlretrieve(BASE_DOWNLOAD_URL+href, downloadLocation+'/'+fileName)
